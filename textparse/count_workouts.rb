a76eca705e4fa7451b1e45cdab086f9fc456a269
Steven Foster <steven@stevenfoster.net>
2019-11-14 19:20:14 -0800

    update to count workouts script

--- textparse/count_workouts.rb
+++ textparse/count_workouts.rb
@@ -3,9 +3,14 @@ workouts = []
 File.open("./workout_journal.txt", "r") do |f|
   f.each_line do |l|
     if l.match(/^\p{L}/)
-    	workouts << l
+    	workouts << l.match(/^[^0-9][A-Za-z ]*/).to_s
     end
   end
 end
 
-puts workouts.size
+workouts.uniq.each do |workout|
+  puts "#{workout}: #{workouts.count(workout)}"
+end
+
+puts "Total Unique Workouts: #{workouts.uniq.size}"
+puts "Total Workouts Overall: #{workouts.size}"
