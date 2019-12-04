19c6974a60ee88a90df2c79ad7bc1fa2ba3b663e
Steven Foster <steven@stevenfoster.net>
2019-12-04 14:46:01 -0800

    added test for palindrome phrase

--- palindrome/stevenfoster_palindrome/test/stevenfoster_palindrome_test.rb
+++ palindrome/stevenfoster_palindrome/test/stevenfoster_palindrome_test.rb
@@ -12,4 +12,8 @@ class StevenfosterPalindromeTest < Minitest::Test
   def test_palindrome
   	assert "madam".palindrome?
   end
+
+  def test_palindrome_phrase
+  	assert "Madam, I'm Adam".palindrome?
+  end
 end
