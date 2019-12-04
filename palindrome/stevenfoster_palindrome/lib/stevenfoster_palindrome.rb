24c7433e38087c356cfdcabde8d75cd5ab2ccfca
Steven Foster <steven@stevenfoster.net>
2019-12-04 14:42:08 -0800

    extended String class to compare if string is equal to its reverse

--- palindrome/stevenfoster_palindrome/lib/stevenfoster_palindrome.rb
+++ palindrome/stevenfoster_palindrome/lib/stevenfoster_palindrome.rb
@@ -1,6 +1,12 @@
 require "stevenfoster_palindrome/version"
 
+class String
+	def palindrome?
+		self == self.reverse
+	end
+end
+
+
 module StevenfosterPalindrome
   class Error < StandardError; end
-  # Your code goes here...
 end
