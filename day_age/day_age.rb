67a8d1bb8af2247f77826fad49b91596bdb34479
Steven Foster <steven@stevenfoster.net>
2020-01-27 14:01:29 -0800

    stubbing out validation for date format and dates in the future

--- day_age/day_age.rb
+++ day_age/day_age.rb
@@ -6,10 +6,13 @@ puts "DD/MM/YYYY"
 
 birthday = STDIN.gets.chomp
 
+# Validate we got a correctly formated date from the user
 birthday = Date.parse(birthday)
 
 today = DateTime.now
 
+# If a user provided a birthday in the future
+# output you will be born in x days!
 day_age = ( today - birthday ).to_i
 
 puts "You are #{day_age} days old today!"
